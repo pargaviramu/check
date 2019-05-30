c358eab73372d084647882f6f02e3c591e89c94f
pargaviramu <50875085+pargaviramu@users.noreply.github.com>
2019-05-30 16:20:21 +0530

    Create vow.py

---
+++ vow.py
@@ -0,0 +1,11 @@
+
+b=input()
+if(b=="a" or b=="e" or b=="i" or b=="o" or b=="u" or b=="A" or b=="E" or b=="I" or b=="O" or b=="U"):
+  print("Vowel")
+
+elif(b.isdigit()):
+  print("Invalid")
+else:
+  print("Consonant")
+
+
